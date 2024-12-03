e305ffd9af3e2a1e7e8c953e36aa70e921f487bc
anilreddy12001 <3403889+anilreddy12001@users.noreply.github.com>
2024-12-03 22:24:09 +0530

    Update unity.ts

--- src/config/unity.ts
+++ src/config/unity.ts
@@ -3,7 +3,7 @@ import { UnityConfig } from '../types/unity';
 // Unity build configuration
 export const UNITY_CONFIG: UnityConfig = {
   loaderUrl: '/Build/WB2.loader.js',//WB2.loader //Build/webgl.loader.js
-  dataUrl: '/Build/data.gz',//Build/webgl.data //
+  dataUrl: '/Build/webgl.data',//Build/webgl.data //
   frameworkUrl: '/Build/build.framework.js',//build.framework.js ///Build/webgl.framework.js
   codeUrl: '/Build/wasm.gz',//build.wasm //webgl.wasm //https://anilproject12001.web.app/build.wasm
 };
