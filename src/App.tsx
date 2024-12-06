6ca98a44b0190e670b1b83f40040c1759a2b53ad
anilreddy12001 <3403889+anilreddy12001@users.noreply.github.com>
2024-12-06 11:09:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,42 @@ import React from 'react';
 import { UnityLoader } from './components/UnityLoader';
 import { Gamepad2 } from 'lucide-react';
 import { UnityProvider } from './contexts/UnityContext';
-
+import "./index.css";
 function App() {
+  let widgetList=[{name:'Widget 1', title:'', type:'', content:{}},{name:'Widget 2', title:'', type:'', content:{}}];
+
+  function handleClickSpawnEnemies() {
+   // sendMessage("GameController", "SpawnEnemies", 100);
+   console.log('clicked button..');
+  }
+
   return (
     <UnityProvider>
+      <div id="overLayWidget">{widgetList.map(e=><><div className="widgetWrapper" style={{border:'1px solid #8e8e8e',padding:'5px'}}>{e.name}</div></>)}</div>
+      {/* <button onClick={handleClickSpawnEnemies}>click</button> */}
       <UnityLoader />
       <div className="min-h-screen bg-gray-100">
         <header className="bg-white shadow-sm">
           <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
             <div className="flex items-center space-x-2">
-              <Gamepad2 className="w-6 h-6 text-blue-500" />
-              <h1 className="text-xl font-bold text-gray-900"></h1>
+              {/* <Gamepad2 className="w-6 h-6 text-blue-500" /> */}
+              <h1 className="text-xl font-bold text-gray-900"> </h1>
             </div>
           </div>
         </header>
 
         <main className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
           <div className="bg-white rounded-lg shadow-xl p-6">
-{/*             <h2 className="text-lg font-semibold text-gray-900 mb-4">Unity Game Window</h2> */}
-{/*             <UnityLoader /> 
-            // <div className="mt-4 text-sm text-gray-600">
-            //   <p>Instructions:</p>
-            //   <ul className="list-disc list-inside">
-            //     <li>Place your Unity WebGL build files in the public/Build directory</li>
-            //     <li>Update the build URLs in UnityContext.tsx</li>
-            //     <li>The game will automatically load when ready</li>
-            //   </ul>
-            // </div>
-            */}
+            <h2 className="text-lg font-semibold text-gray-900 mb-4">Unity Game Window</h2>
+            {/* <UnityLoader /> */}
+            {/* <div className="mt-4 text-sm text-gray-600">
+              <p>Instructions:</p>
+              <ul className="list-disc list-inside">
+                <li>Place your Unity WebGL build files in the public/Build directory</li>
+                <li>Update the build URLs in UnityContext.tsx</li>
+                <li>The game will automatically load when ready</li>
+              </ul>
+            </div> */}
           </div>
         </main>
       </div>
